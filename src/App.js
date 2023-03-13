7dffb7ab59a17061cec0aea2f912a017c0f00304
Abhishek Kumar <abhishekkumar.in047@gmail.com>
2023-03-13 17:36:56 +0530

    done

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Home from './components/Home';
 function App() {
   return (
     <div className="App flex bg-amber-400 text-white m-0 h-screen w-screen">
-
       <Home/>
-
     </div>
   );
 }
